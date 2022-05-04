df508456b2579868e8c29c3ade5f73befdb187e2
Niels Otterspoor <niels.otterspoor@gmail.com>
2022-05-04 16:51:17 +0200

    added prettier

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-module.exports = {
-    // eslint-configs
-    extends: ['airbnb/base', 'plugin:prettier/recommended'],
-    // eslint-plugins
-    plugins: ['simple-import-sort'],
-    // enabling/disabling/changing level of rules
-    rules: {
-      'class-methods-use-this': 'off',
-      'import/order': 'warn',
-      'import/prefer-default-export': 'off',
-      'no-plusplus': 'off',
-      'no-restricted-globals': 'off',
-      'prettier/prettier': [
-        'error',
-        // configure Prettier for ESLint
-        {
-          arrowParens: 'avoid',
-          printWidth: 100,
-          singleQuote: true,
-          trailingComma: 'all',
-        },
-      ],
-    },
-  };
+const eslintrc = require('./js/.eslintrc.js');
 
-  // We can omit the eslint-config- prefix in our package's name. Eg if our package is called eslint-config-example:
-  // {
-  //   "extends": "nossie"
-  // }
+module.exports = eslintrc;
---
+++ js/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // eslint-configs
+  extends: ['airbnb/base', 'plugin:prettier/recommended'],
+  // eslint-plugins
+  plugins: ['simple-import-sort'],
+  // enabling/disabling/changing level of rules
+  rules: {
+    'class-methods-use-this': 'off',
+    'import/order': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-plusplus': 'off',
+    'no-restricted-globals': 'off',
+    'prettier/prettier': [
+      'error',
+      // configure Prettier for ESLint
+      {
+        arrowParens: 'avoid',
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
+
+// We can omit the eslint-config- prefix in our package's name. Eg if our package is called eslint-config-example:
+// {
+//   "extends": "nossie"
+// }
